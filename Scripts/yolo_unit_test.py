e9471b90495af2b5b9013e7c1258afd55661ef4c
S1MS4 <arijussimasius@gmail.com>
2025-04-04 20:37:43 +0300

    unit testing
    added unit testing to divided scripts

---
+++ Scripts/yolo_unit_test.py
@@ -0,0 +1,30 @@
+import unittest
+import numpy as np
+from Scripts.app import YOLOModelFactory, YOLOTracker, VideoHandler
+
+class TestYOLOIntegration(unittest.TestCase):
+    def setUp(self):
+        self.model = YOLOModelFactory.create("yolo11m.pt")
+        self.tracker = YOLOTracker(self.model)
+
+    def test_model_creation(self):
+        self.assertIsNotNone(self.model)
+
+    def test_process_frame(self):
+        frame = np.zeros((640, 480, 3), dtype=np.uint8) # empty image
+        results = self.tracker.process_frame(frame)
+        self.assertIsNotNone(results)
+
+    def test_update_history(self):
+        boxes = np.array([[100, 100, 50, 50]])
+        track_ids = [1]
+        self.tracker.update_history(boxes, track_ids)
+        self.assertIn(1, self.tracker.history)
+
+    def test_video_handler(self):
+        handler = VideoHandler("../test_video.mp4")
+        self.assertTrue(handler.cap.isOpened())
+        handler.release()
+
+if __name__ == "__main__":
+    unittest.main()
